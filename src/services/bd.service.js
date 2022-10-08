144220ce3d969d92b1eec0d5a1285cb3dcf0adb0
EmersonCcp <emerson.paixao52@gmail.com>
2022-10-07 21:52:33 -0300

    dd

--- src/services/bd.service.js
+++ src/services/bd.service.js
@@ -1,9 +1,6 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('tapiceria', 'postgres', '9014', {
-    host: 'localhost',
-    port: 5432,
-    dialect: 'postgres'
-  });
+const URI = "postgres://rxkvxalocewcic:cd988e9def77192422b2cc5f370d649cebe069f9dfa46433c8a124b47527b54f@ec2-3-219-19-205.compute-1.amazonaws.com:5432/da1gcoaqplhlgr";
+const sequelize = new Sequelize(URI);
 
   const testConnection = async () =>{
      try {
